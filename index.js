f58ebd5d7078c18f62978fbad99902f7ae2881da
jaremsalonga <39017556+jaremsalonga@users.noreply.github.com>
2022-05-27 00:31:23 +0800

    tetu

--- index.js
+++ index.js
@@ -28,11 +28,15 @@ const saltRounds = 10;
 
 
 app.use(express.json());
-app.use(cors({
-    origin: ['*'],
-    methods: ["GET", "POST", "DELETE"],
-    credentials: true
-}));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
